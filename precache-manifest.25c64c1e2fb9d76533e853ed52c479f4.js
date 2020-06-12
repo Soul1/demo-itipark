3756ea7fc13e221906618ef7ed5e038509de4a45
Soul1 <darkwaud@gmail.com>
2020-06-12 10:11:16 +0300

    Updates

---
+++ precache-manifest.25c64c1e2fb9d76533e853ed52c479f4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3253b1cfb4d3984a06066e8aad306777",
+    "url": "/demo-itipark/index.html"
+  },
+  {
+    "revision": "622c1460a8e97a05be8a",
+    "url": "/demo-itipark/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "dfd44b4b8d573bb1e911",
+    "url": "/demo-itipark/static/css/main.406bb4d1.chunk.css"
+  },
+  {
+    "revision": "622c1460a8e97a05be8a",
+    "url": "/demo-itipark/static/js/2.7ad1c794.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/demo-itipark/static/js/2.7ad1c794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd44b4b8d573bb1e911",
+    "url": "/demo-itipark/static/js/main.788bbaa5.chunk.js"
+  },
+  {
+    "revision": "03d11c60e1f177f1faa6",
+    "url": "/demo-itipark/static/js/runtime-main.5f90f460.js"
+  },
+  {
+    "revision": "4bbefc47854ab73008743ec383d71b73",
+    "url": "/demo-itipark/static/media/clock.4bbefc47.svg"
+  },
+  {
+    "revision": "f38199498d2673a256bc86da8dee129b",
+    "url": "/demo-itipark/static/media/email.f3819949.svg"
+  },
+  {
+    "revision": "34acafaa22fd3e7f15fd85ca62e52498",
+    "url": "/demo-itipark/static/media/footer-contact.34acafaa.jpg"
+  },
+  {
+    "revision": "72ab5d5d8c12731d8abec5a64b62d33c",
+    "url": "/demo-itipark/static/media/footer-email.72ab5d5d.svg"
+  },
+  {
+    "revision": "ab28c668bd041165bb2554cc6f898bd9",
+    "url": "/demo-itipark/static/media/footer-phone.ab28c668.svg"
+  },
+  {
+    "revision": "dcd871aaf3d81299c267d99d200a0c1d",
+    "url": "/demo-itipark/static/media/header-slider-bg.dcd871aa.jpg"
+  },
+  {
+    "revision": "6e8f186e3bb70efc8e14c50d48d431b7",
+    "url": "/demo-itipark/static/media/logo.6e8f186e.svg"
+  },
+  {
+    "revision": "3a2648d6b6dd44f2a675b1d0241e8fe8",
+    "url": "/demo-itipark/static/media/map.3a2648d6.svg"
+  },
+  {
+    "revision": "321338f23c9d26f36066f8a7ebd3fe01",
+    "url": "/demo-itipark/static/media/next.321338f2.svg"
+  },
+  {
+    "revision": "bde9827be4f910c703acc7a66749d5ec",
+    "url": "/demo-itipark/static/media/phone.bde9827b.svg"
+  },
+  {
+    "revision": "79a7e58003edd5f93a71776a06e5725f",
+    "url": "/demo-itipark/static/media/prev.79a7e580.svg"
+  }
+]);
